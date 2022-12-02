ad22c3576d60d348402082bdad1ab9b8d75209b5
lrezendes <lrezendes@student.bridgew.edu>
2022-12-02 17:28:39 -0500

    Initial commit

---
+++ src/MessageFragment.java
@@ -0,0 +1,10 @@
+public class MessageFragment {
+    char letter;
+    MessageFragment nextNode;
+
+    public MessageFragment (char newLetter, MessageFragment newMessageFragment) {
+	this.letter = newLetter;
+    this.nextNode = newMessageFragment;
+    }
+
+}
